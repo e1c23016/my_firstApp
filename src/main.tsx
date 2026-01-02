557af5deea336b9a2f1d860fc6f8e0e9ade90363
e1c23016 <e1c23016@oit.ac.jp>
2026-01-03 00:32:00 +0900

    位置情報取得が確認できた、APIがあることの確認ができた

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// const hotpepperApiKey = import.meta.env.VITE_HOTPEPPER_API_KEY;
+// console.log("Hot Pepper API Key:", hotpepperApiKey);
+// const hotpepperApiUrl = import.meta.env.VITE_HOTPEPPER_API_URL;
+// console.log("Hot Pepper API URL:", hotpepperApiUrl);
